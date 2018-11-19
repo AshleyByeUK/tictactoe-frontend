dd443bbdf94f62a2fcf385223727f4b9344a1598
Ashley Bye <ashley+git@ashleybye.uk>
2018-11-19 15:06:16 +0000

    dynamically determine game service endpoint

--- src/index.js
+++ src/index.js
@@ -4,7 +4,13 @@ import GameService from './game/game-service';
 import App from './app';
 import './index.css'
 
-const API_URL = 'http://localhost:9292/api/v1'
+let API_URL;
+if (process.env.NODE_ENV === "production") {
+  API_URL = 'https://ruby-tictactoe-api.herokuapp.com/api/v1'
+} else {
+  API_URL = 'http://localhost:9292/api/v1';
+}
+
 const gameService = new GameService(API_URL);
 
 ReactDOM.render(
